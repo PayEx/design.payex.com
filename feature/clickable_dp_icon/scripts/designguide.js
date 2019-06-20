34f502e413a95ecd0ad956eacfdc3d9109e40249
payex-dev <dev@payex.com>
2019-06-20 09:54:33 +0000

    GitHub release 2.3.0-clickable-dp-icon.1+32

---
+++ feature/clickable_dp_icon/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[5],{353:function(p,n,w){}}]);
+//# sourceMappingURL=designguide.js.map?ef4b48a72143655c8006
---
+++ feature/clickable_dp_icon/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[60],{352:function(p,n,w){}}]);
+//# sourceMappingURL=documentation.js.map?ef4b48a72143655c8006

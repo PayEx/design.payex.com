92e74288725adc2a1e7fdf28dd312b8fab99a59c
payex-dev <dev@payex.com>
2019-11-04 15:06:39 +0000

    GitHub release 2.4.0-fix-snapshot.1+5

---
+++ fix/snapshot/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[5],{353:function(p,n,w){}}]);
+//# sourceMappingURL=designguide.js.map?82e085e893e3e2abb4ad
---
+++ fix/snapshot/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[64],{351:function(p,n,w){}}]);
+//# sourceMappingURL=px.js.map?82e085e893e3e2abb4ad

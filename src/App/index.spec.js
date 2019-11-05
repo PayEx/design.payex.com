626affa0ce225bc0744f1c62d4e3a2612175ef67
Thomas Lorntzen <cyrthomas1@gmail.com>
2019-11-05 11:40:21 +0100

    Fix a testcafe async test.

--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -1,11 +1,12 @@
-import { Selector } from "testcafe";
+import {
+    Selector
+} from "testcafe";
 
 fixture `Frontpage`
     .page `http://localhost:3000`;
 
-test("Frontpage test", async t => {
+test("Frontpage test", async () => {
     const header = await Selector(".panel-body").find("h2");
 
-    await t
-        .expect(header.textContent).contains("Welcome to the PayEx DesignGuide");
+    expect(header.textContent).contains("Welcome to the PayEx DesignGuide");
 });
